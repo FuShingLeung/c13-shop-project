d0e25d8b001cefc33c77e3df2f45318ff96817b5
Fu Shing Leung <bobbyfs123@gmail.com>
2023-05-09 23:38:14 +0100

    install mui

---
+++ components/mui/index.ts
@@ -0,0 +1,4 @@
+import Button from '@mui/material/Button';
+import EditIcon from '@mui/icons-material/Edit';
+
+export { Button, EditIcon };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '@/lib/theme';
+import { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
